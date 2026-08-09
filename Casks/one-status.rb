6d042921dda5b41fcf951f1b7c3fe195f0b0428f
niyuxuan782 <1648428764@qq.com>
2026-08-09 12:48:09 +0800

    fix: version Homebrew cask URLs

--- Casks/one-status.rb
+++ Casks/one-status.rb
@@ -4,12 +4,12 @@ cask "one-status" do
   on_arm do
     sha256 "c82c24d273c55fdafd49a8edb5227f9eacfbdac1b3733c9985db3cfad412a074"
 
-    url "https://github.com/niyuxuan782/one-status/releases/download/v0.3.0/One-Status-0.3.0-mac-arm64.dmg"
+    url "https://github.com/niyuxuan782/one-status/releases/download/v#{version}/One-Status-#{version}-mac-arm64.dmg"
   end
   on_intel do
     sha256 "dad497d7a961fd3e835e36716c95b8d5a058321572cefddf010f2fa4e324c969"
 
-    url "https://github.com/niyuxuan782/one-status/releases/download/v0.3.0/One-Status-0.3.0-mac-x64.dmg"
+    url "https://github.com/niyuxuan782/one-status/releases/download/v#{version}/One-Status-#{version}-mac-x64.dmg"
   end
 
   name "One Status"
